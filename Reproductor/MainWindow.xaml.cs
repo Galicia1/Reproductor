a6ae22077430f99db7bd0ec5d3629d5b2cac6f28
Galicia <jorgeluis.gv@hotmail.com>
2019-02-22 12:35:27 -0700

    Reproduce musica

--- Reproductor/MainWindow.xaml.cs
+++ Reproductor/MainWindow.xaml.cs
@@ -12,8 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
 
 using Microsoft.Win32;
+
+
 
 namespace Reproductor
 {
@@ -22,9 +26,26 @@ namespace Reproductor
     /// </summary>
     public partial class MainWindow : Window
     {
+        AudioFileReader reader;
+        //Nuestra comunicacion con la tarjeta de sonido
+        WaveOutEvent output;
+
         public MainWindow()
         {
             InitializeComponent();
+            LLenarComboSalida();
+        }
+
+        private void LLenarComboSalida()
+        {
+            cbSalida.Items.Clear();
+            for(int i=0; i< WaveOut.DeviceCount; i++)
+            {
+                WaveOutCapabilities capacidades = WaveOut.GetCapabilities(i);
+                cbSalida.Items.Add(capacidades.ProductName);
+
+            }
+            cbSalida.SelectedIndex = 0;
         }
 
         private void btnElegirArchivo_Click(object sender, RoutedEventArgs e)
@@ -36,6 +57,65 @@ namespace Reproductor
                 txtRutaArchivo.Text =
                      openFileDialog.FileName;
             }
+        }
+
+        private void btnReproducir_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != null && output.PlaybackState == PlaybackState.Paused)
+            {
+                output.Play();
+                btnReproducir.IsEnabled = false;
+                btnPausa.IsEnabled = true;
+                btnDetener.IsEnabled = true;
+            }
+            else
+            {
+                reader = new AudioFileReader(txtRutaArchivo.Text);
+                output = new WaveOutEvent();
+
+                output.DeviceNumber = cbSalida.SelectedIndex;
+
+                output.PlaybackStopped += Output_PlaybackStopped;
+
+                output.Init(reader);
+                output.Play();
+
+                btnDetener.IsEnabled = true;
+                btnPausa.IsEnabled = true;
+                btnReproducir.IsEnabled = false;
+
+                //tiempo final
+                lblTiempoActual.Text = reader.TotalTime.ToString().Substring(0,8);
+            }
+            
+
+        }
+
+        private void Output_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            reader.Dispose();
+            output.Dispose();
+        }
+
+
+        private void btnPausa_Click(object sender, RoutedEventArgs e)
+        {
+            if (output!= null)
+            {
+                output.Pause();
+                btnDetener.IsEnabled = true;
+                btnPausa.IsEnabled = false;
+                btnReproducir.IsEnabled = true;
+
+            }
+        }
+
+        private void btnDetener_Click(object sender, RoutedEventArgs e)
+        {
+            output.Stop();
+            btnReproducir.IsEnabled = true;
+            btnPausa.IsEnabled = false;
+            btnDetener.IsEnabled = false;
         }
     }
 }
